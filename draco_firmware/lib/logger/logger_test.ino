fdfc09fa84209ef59c7c5b314dc4c6db45e10162
Vlad Pomogaev <vlad@AD.IZABER.COM>
2018-03-12 10:19:51 -0700

    testing platformio

--- draco_firmware/lib/logger/logger_test.ino
+++ draco_firmware/lib/logger/logger_test.ino
@@ -1,12 +1,13 @@
+#include <Arduino.h>
 #include "logger.h"
 #include "constants.h"
 
-logger telemetry_logger = new logger("logger_test_file.txt");
+logger telemetry_logger("logger_test_file.txt");
 
 void setup(){
     Serial.begin(115200);
     pinMode(LVL_EN, OUTPUT);
-    digtialWrite(LVL_EN, HIGH);
+    digitalWrite(LVL_EN, HIGH);
 
     //Add variables to the logger
     telemetry_logger.addVariable(&millis());
